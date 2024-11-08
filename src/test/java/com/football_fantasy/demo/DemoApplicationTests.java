964347b276d2c2d0e0ee8804e74a28bbb0b89d94
marko <marko@SERGEJ>
2024-11-08 13:05:47 +0100

    Initial commit

---
+++ src/test/java/com/football_fantasy/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football_fantasy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
